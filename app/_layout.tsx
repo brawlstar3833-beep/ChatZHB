ae761e8d45f6de98cceb858be3f2a543cff3b262
brawlstar3833-beep <brawlstar3833@gmail.com>
2026-04-26 23:38:33 +0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,13 @@
-import "../global.css";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" backgroundColor="#09090b" />
+      <StatusBar style="auto" />
       <Stack
         screenOptions={{
           headerShown: false,
-          contentStyle: { backgroundColor: "#09090b" },
           animation: "fade",
         }}
       />
